d621f365cb1f67f85342a90d60cfe7e4386a5758
kokoro higuchi <ice.choco.pudding.kokoro@gmail.com>
2023-02-12 14:55:26 +0900

    Fix imports

--- tests/test_greet.py
+++ tests/test_greet.py
@@ -1,8 +1,9 @@
-from my_package.greet import hello
+from src.my_package.greet import hello
 
 
 def test_hello():
     import sys
     from pprint import pprint
+
     pprint(sys.path)
     assert hello() == "Hello."
